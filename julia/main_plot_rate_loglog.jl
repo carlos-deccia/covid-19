25cb0ad2c3ee6aed0a91dbea6d55d7990399bb39
Carlos <carlos.deccia@colorado.edu>
2020-03-31 11:36:00 -0600

    save before changing the rate loglog code

--- julia/main_plot_rate_loglog.jl
+++ julia/main_plot_rate_loglog.jl
@@ -22,13 +22,68 @@ datestrings = Dates.format.(time_span_days, "u dd")
 conf_Country = conf_def[:,2]
 countries_unique = unique(conf_def[:,2])
 ###
-global_rate_CITUU_7days = true
-if global_rate_CITUU_7days
+
+###
+# #
+# global_rate_CITUU_7days_old = false
+# if global_rate_CITUU_7days_old
+# delay_days = 7
+# for idx_c = 1:length(countries_unique)
+#     if occursin("China",countries_unique[idx_c]) || occursin("Italy",countries_unique[idx_c]) || occursin("Spain",countries_unique[idx_c]) || occursin("Turkey",countries_unique[idx_c]) || occursin("Uruguay",countries_unique[idx_c]) || occursin("US",countries_unique[idx_c])
+#         indx_tmp_c = findall(isequal(countries_unique[idx_c]),conf_Country)
+#
+#         rate_data = zeros(length(datestrings)-delay_days)
+#         rate_data_new = zeros(length(datestrings)-delay_days-1)
+#         for i = length(datestrings)-delay_days:-1:delay_days+1
+#             rate_data[i] = sum(Data_matrix_conf[indx_tmp_c,i-delay_days:i])
+#         end
+#         for ii = 1:length(rate_data)-1
+#         rate_data_new[ii] = rate_data[ii+1] - rate_data[ii]
+#         end
+#
+#         total_data = sum(Data_matrix_conf[indx_tmp_c,1:length(datestrings)-delay_days-1],dims=1)'
+#
+#         Data_entries = countries_unique[idx_c]
+#         plot!(total_data,rate_data_new, xscale = :log10, xlims = (1, 10^6), yscale = :log10, ylims = (1, 10^6), xlabel="Total cases [-]",  ylabel="Rate of cases [per 3 days]", labels=Data_entries, legend=:topleft, dpi=300, show=true)
+#     end
+# end
+# savefig(string(save_figs_folder,"global_rate_CISTUU_$delay_days","days.png"))
+# end
+# # #
+# #
+# global_rate_CITUU_3days_old = false
+# if global_rate_CITUU_3days_old
+# delay_days = 3
+# for idx_c = 1:length(countries_unique)
+#     if occursin("China",countries_unique[idx_c]) || occursin("Italy",countries_unique[idx_c]) || occursin("Spain",countries_unique[idx_c]) || occursin("Turkey",countries_unique[idx_c]) || occursin("Uruguay",countries_unique[idx_c]) || occursin("US",countries_unique[idx_c])
+#         indx_tmp_c = findall(isequal(countries_unique[idx_c]),conf_Country)
+#
+#         rate_data = zeros(length(datestrings)-delay_days)
+#         rate_data_new = zeros(length(datestrings)-delay_days-1)
+#         for i = length(datestrings)-delay_days:-1:delay_days+1
+#             rate_data[i] = sum(Data_matrix_conf[indx_tmp_c,i-delay_days:i])
+#         end
+#         for ii = 1:length(rate_data)-1
+#         rate_data_new[ii] = rate_data[ii+1] - rate_data[ii]
+#         end
+#
+#         total_data = sum(Data_matrix_conf[indx_tmp_c,1:length(datestrings)-delay_days-1],dims=1)'
+#
+#         Data_entries = countries_unique[idx_c]
+#         plot!(total_data,rate_data_new, xscale = :log10, xlims = (1, 10^6), yscale = :log10, ylims = (1, 10^6), xlabel="Total cases [-]",  ylabel="Rate of cases [per 3 days]", labels=Data_entries, legend=:topleft, dpi=300, show=true)
+#     end
+# end
+# savefig(string(save_figs_folder,"global_rate_CISTUU_$delay_days","days.png"))
+# end
+
+##
+# global_rate_CITUU_7days = true
+# if global_rate_CITUU_7days
 delay_days = 7
 for idx_c = 1:length(countries_unique)
     if occursin("China",countries_unique[idx_c]) || occursin("Italy",countries_unique[idx_c]) || occursin("Spain",countries_unique[idx_c]) || occursin("Turkey",countries_unique[idx_c]) || occursin("Uruguay",countries_unique[idx_c]) || occursin("US",countries_unique[idx_c])
         indx_tmp_c = findall(isequal(countries_unique[idx_c]),conf_Country)
-# countries_unique[78]
+        # countries_unique[78] [33]
 
         rate_data = zeros(timesteps-delay_days)
         rate_data_new = zeros(timesteps-delay_days-1)
@@ -39,45 +94,12 @@ for idx_c = 1:length(countries_unique)
         rate_data_new[ii] = rate_data[ii+1] - rate_data[ii]
         end
 
-        total_data = zeros(timesteps)
-        for i=1:timesteps
-            total_data[i] = sum(Data_matrix_conf[indx_tmp_c,1:i])
-        end
+        # total_data = zeros(timesteps-delay_days-1)
+        total_data = sum(Data_matrix_conf[indx_tmp_c,end-(timesteps-delay_days-1)+1:end],dims=1)'
 
         Data_entries = countries_unique[idx_c]
-        plot!(total_data[delay_days+2:timesteps],rate_data_new, xscale = :log10, xlims = (10, 10^7), yscale = :log10, ylims = (1, 10^6), xlabel="Total cases [-]",  ylabel="Rate of cases [per 3 days]", labels=Data_entries, legend=:topleft, dpi=300, show=true)
+        plot!(total_data,rate_data_new, xscale = :log10, xlims = (10, 10^7), yscale = :log10, ylims = (1, 10^6), xlabel="Total cases [-]",  ylabel="Rate of cases [per 3 days]", labels=Data_entries, legend=:topleft, dpi=300, show=true)
     end
 end
-savefig(string(save_figs_folder,"global_rate_CISTUU_7days.png"))
-end
-###
-#
-global_rate_CITUU_3days = false
-if global_rate_CITUU_3days
-delay_days = 3
-for idx_c = 1:length(countries_unique)
-    if occursin("China",countries_unique[idx_c]) || occursin("Italy",countries_unique[idx_c]) || occursin("Spain",countries_unique[idx_c]) || occursin("Turkey",countries_unique[idx_c]) || occursin("Uruguay",countries_unique[idx_c]) || occursin("US",countries_unique[idx_c])
-        indx_tmp_c = findall(isequal(countries_unique[idx_c]),conf_Country)
-# countries_unique[78]
-
-        rate_data = zeros(timesteps-delay_days)
-        rate_data_new = zeros(timesteps-delay_days-1)
-        for i = timesteps-delay_days:-1:delay_days+1
-            rate_data[i] = sum(Data_matrix_conf[indx_tmp_c,i-delay_days:i])
-        end
-        for ii = 1:length(rate_data)-1
-        rate_data_new[ii] = rate_data[ii+1] - rate_data[ii]
-        end
-
-        total_data = zeros(timesteps)
-        for i=1:timesteps
-            total_data[i] = sum(Data_matrix_conf[indx_tmp_c,1:i])
-        end
-
-        Data_entries = countries_unique[idx_c]
-        plot!(total_data[delay_days+2:timesteps],rate_data_new, xscale = :log10, xlims = (10, 10^7), yscale = :log10, ylims = (1, 10^6), xlabel="Total cases [-]",  ylabel="Rate of cases [per 3 days]", labels=Data_entries, legend=:topleft, dpi=300, show=true)
-    end
-end
-savefig(string(save_figs_folder,"global_rate_CISTUU_3days.png"))
-end
-#
+savefig(string(save_figs_folder,"global_rate_CISTUU_7days_new.png"))
+# end
